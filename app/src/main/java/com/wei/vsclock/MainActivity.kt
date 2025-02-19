8e1712200135e8d1a5528e1e1ac4061f49bcf44c
WeiHe <azrael8576love2008@gmail.com>
2025-02-20 04:59:07 +0800

    chore(designsystem): Implement Gilroy font and establish App Theme

--- app/src/main/java/com/wei/vsclock/MainActivity.kt
+++ app/src/main/java/com/wei/vsclock/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.wei.vsclock.ui.theme.VsclockTheme
+import com.wei.vsclock.core.designsystem.theme.VsclockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             VsclockTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "VsClock",
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     VsclockTheme {
-        Greeting("Android")
+        Greeting("VsClock")
     }
 }
